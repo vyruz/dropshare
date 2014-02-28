e97e40232dc9a3d316a5644e48898e1f5c4b0920
Max Harris <mjharris@colorado.edu>
2014-02-28 16:49:49 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dropshare::Application.config.secret_key_base = '6cab7dc9b323cc6da7ea0df3218198a013f647ffbd63bc498bb676f7cbf271c18251603e8b77a4c809dc19562a7e0e3b953cd34a10ba68123ec526d3977db516'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dropshare::Application.config.session_store :cookie_store, key: '_dropshare_session'
