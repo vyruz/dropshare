dddf5b313e7a3616b6d9b583c280a9f6a4361bce
Max Harris <mjharris@colorado.edu>
2014-03-03 12:47:49 -0700

    add 'full_title' method to new rspec utilities file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,9 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
+  # Use capybara for english-like phrases in rspecs
+  config.include Capybara::DSL
+  
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
