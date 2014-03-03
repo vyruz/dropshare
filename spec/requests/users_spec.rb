a8e0da01a20fd8de39c0bd4e3073baf1f3942ae6
Max Harris <mjharris@colorado.edu>
2014-03-03 12:09:58 -0700

    add some integration tests for signup page

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe "Users" do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get signup_path
-      response.status.should be(200)
-    end
+  subject { page }
+  
+  describe "signup page" do
+    before { visit signup_path }
+    
+    it { should have_content('Sign Up') }
+    it { should have_title(full_title('Sign up')) }
   end
 end
