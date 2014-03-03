d35e622d2dbf7e559c5874c34b1874c30864bb4e
Max Harris <mjharris@colorado.edu>
2014-03-03 12:05:16 -0700

    add rspec integration test for users & root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Dropshare::Application.routes.draw do
-  get "users/new"
+  root 'users#new'
+  match '/signup', to: 'users#new', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
