be8e67beedb9dbf5d66438fa70c1c55c104c5fed
Max Harris <mjharris@colorado.edu>
2014-03-31 17:56:16 -0600

    add migration file for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140324195226) do
+ActiveRecord::Schema.define(version: 20140331234849) do
 
   create_table "sessions", force: true do |t|
     t.datetime "created_at"
