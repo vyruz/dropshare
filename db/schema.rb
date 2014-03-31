d21a3cca3bfc12fd6078c0402d958bbdb69d0a8d
Max Harris <mjharris@colorado.edu>
2014-03-31 17:11:23 -0600

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140308033303) do
+ActiveRecord::Schema.define(version: 20140324195226) do
+
+  create_table "sessions", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "facebook"
+    t.string   "uid"
   end
 
 end
