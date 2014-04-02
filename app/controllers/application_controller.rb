97b94310324d0a8e42e762acdc50aa4b757f67fb
Luke Woodruff <lwvyruz@gmail.com>
2014-04-02 17:05:52 -0600

    added current_drive similar to current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def current_drive
+    @current_drive ||= User.find(session[:drive]) if session[:drive]
+  end
   
   helper_method :current_user
 end
